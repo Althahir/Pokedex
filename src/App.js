09e97b4d1a4b4ab236754ce591cecfb5e4e874b1
althahir <romain.sebastiani@gmail.com>
2026-01-30 23:30:29 +0100

    Ajout de la team

--- src/App.js
+++ src/App.js
@@ -10,12 +10,41 @@ import './App.css';
 import pkmnList from "./pokedex.json"
 import PokemonList from './PokemonList';
 import logo from  './logo.png'
-import { useState } from 'react';
-
-
+import { useEffect, useState } from 'react';
+import logoRec from './logoRecherche.png'
 
 
 function App() {
+  const [recherche,setRecherche]=useState("")
+  const [listFav,setListFav]=useState([])
+
+    // listFav.length>0 ? console.log(listFav) : console.log("")
+    useEffect(() => {
+    if (listFav.length > 0) {
+      // console.log("--- Mes Favoris ---");
+      listFav.forEach(id => {
+      // On cherche l'objet pokemon complet dans la liste d'origine
+      const pokemonTrouve = pkmnList.find(p => p.id === id);
+      
+      if (pokemonTrouve) {
+        // console.log(`ID ${id} : ${pokemonTrouve.nom}`);
+        localStorage.setItem=("PkmnFav",JSON.stringify)
+      }
+    });
+  }
+}, [listFav]);
+
+  const favPokemon=(id)=>{
+    if (listFav.includes(id)){
+      const nvFab = listFav.filter(favId=> favId !== id)
+      
+      setListFav(nvFab)
+    }else{
+      setListFav([...listFav,id]);
+      
+
+    }
+  }
 
   
   const pkmnNew=pkmnList.map(el=>{
@@ -23,23 +52,59 @@ function App() {
   <div>
   <PokemonList 
     key={el.id} 
-      // id={el.id} 
+      id={el.id} 
       nom={el.nom} 
       type={el.type} 
-      image={el.image} />
+      image={el.image}
+      isFav={listFav.includes(el.id)}
+      onFavClick={favPokemon} />
       
-      </div>
+    </div>
     )
 })
-  
+  const afficherFav=()=>{
+    const listAffichee = listFav.map(el=>{
+      const pk= pkmnList.find(p => p.id===el)
+      if (pk){
+        return(
+          <PokemonList 
+          key={pk.id} 
+          id={pk.id} 
+          nom={pk.nom} 
+          type={pk.type} 
+          image={pk.image}
+          isFav={true} // Ils sont forcément favoris ici
+          onFavClick={favPokemon} 
+        />
+        )
+      }
+      return null;
+    })
+    return listAffichee
+  }
 return (
     <div className="App">
       <img className="logo" src={logo}></img>
-      {/* <div className='search'>
-        <input className='searchPkmn'></input>
-        <button className='searchButton'>Rechercher</button>
-      </div> */}
+      <div className='search'>
+        <input className='searchPkmn' placeholder='Entrer un nom'></input>
+        <button id="searchButton" className='searchButton'>Recherche</button>
+        <p id='message'></p>
+      </div>
+      <h2>Mon équipe ({listFav.length})</h2>
+      <div className='equipe'>
+        {listFav.length > 0 
+          ? (
+            <div className="equipe-container">
+              {afficherFav()}
+            </div>
+            )
+          : (
+            <p>Aucun Pokémon dans votre équipe pour le moment.</p>
+            )}
+      </div>
       <div className='body'>
+
+           
      {pkmnNew}
      </div>
     </div>
--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -2,18 +2,23 @@ import React from "react"
 import { useState } from "react"
 
 
-const PokemonList =({id,nom,type,image})=>{
+const PokemonList =({id,nom,type,image, isFav, onFavClick})=>{
  
  
  
     return(
         
         <div className="cardPkmn">
-           {/* <p className="idPkmn">Id : {id}</p> */}
-           <p className="nomPkmn">{nom}</p>
-           <p className={`type ${type}`}>{type}</p>
-           <img src={image} className="imagePkmn" alt="" />
-           {/* <button className="buttonPkmn"  onClick={SelectFav}>{isFav==false ? `Mettre en favoris` : `Enlever des favoris`}</button> */}
+            <p className="idPkmn">Id : {id}</p>
+            <p className="nomPkmn">{nom}</p>
+            <img src={image} className="imagePkmn" alt="" />
+            <p className={`type ${type}`}>{type}</p>
+            {/* <button className="buttonPkmn" onClick={}>Ajouter au favoris</button> */}
+            <button className="btnPkmn" onClick={() => onFavClick(id)}>
+                {isFav 
+                    ? <p className="isFav">Dans les favs ! <br></br><span>⭐</span></p>
+                    : <p className="isNotFav">Ajouter aux favs ? <br></br><span>☆</span></p>}
+            </button>
           
         </div>
        
